d873c4c7e9dda561c88c8668aa8e83c286791a89
LEEHYEYEONG <elto789@naver.com>
2024-03-01 20:59:57 +0900

    feat: delete 버튼 추가

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -4,32 +4,48 @@ import { IToDo, toDoState, Categories } from "../atoms";
 
 function ToDo({ text, category, id }: IToDo) {
   const setToDos = useSetRecoilState(toDoState);
-  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+
+  const onChangeClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     const {
       currentTarget: { name },
     } = event;
-    setToDos(oldToDos => {
-      const targetIndex = oldToDos.findIndex(toDo => toDo.id === id)
-      const oldToDo = oldToDos[targetIndex];
+    setToDos((oldToDos) => {
+      const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
       const newToDo = { text, id, category: name as any };
       return [
         ...oldToDos.slice(0, targetIndex),
         newToDo,
-        ...oldToDos.slice(targetIndex + 1)]
-    })
+        ...oldToDos.slice(targetIndex + 1),
+      ];
+    });
   };
+
+  const onDeleteClick = () => {
+    setToDos((oldToDos) => {
+      const newToDos = oldToDos.filter((todo) => todo.id !== id);
+      return newToDos;
+    });
+  };
+
   return (
     <li>
       <span>{text}</span>
       {category !== Categories.DOING && (
-        <button name={Categories.DOING} onClick={onClick}>Doing</button>
+        <button name={Categories.DOING} onClick={onChangeClick}>
+          Doing
+        </button>
       )}
       {category !== Categories.TO_DO && (
-        <button name={Categories.TO_DO} onClick={onClick}>To Do</button>
+        <button name={Categories.TO_DO} onClick={onChangeClick}>
+          To Do
+        </button>
       )}
       {category !== Categories.DONE && (
-        <button name={Categories.DONE} onClick={onClick}>Done</button>
+        <button name={Categories.DONE} onClick={onChangeClick}>
+          Done
+        </button>
       )}
+      <button onClick={onDeleteClick}>Delete</button>
     </li>
   );
 }
